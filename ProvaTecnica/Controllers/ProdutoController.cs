9daba2517799860286c76e78d6c0d016df1b44fe
gustaasouzaa <gustavo_souzapr@outlook.com>
2025-08-13 22:44:44 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaTecnica/Controllers/ProdutoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProvaTecnica.Data;
+using ProvaTecnica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProvaTecnica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Endpoint GET /api/produto
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+
+        // Endpoint POST /api/produto
+        [HttpPost]
+        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProdutos), new { id = produto.Id }, produto);
+        }
+    }
+}
---
+++ ProvaTecnica/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProvaTecnica.Models;
+
+namespace ProvaTecnica.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
